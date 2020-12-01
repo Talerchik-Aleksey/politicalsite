e776cd7c7d4a4309b9d6b657e68c071ac3340cea
Talerchik-Aleksey <talerchik.al@gmail.com>
2020-12-01 20:50:41 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-let project_folder = "dist";
+let project_folder = require("path").basename(__dirname);
 let source_folder = "src";
+
+let fs = require("fs");
 
 let path = {
   build: {
@@ -125,25 +127,25 @@ function images() {
 }
 
 function fonts(params) {
-  src(path.src.fonts)
-    .pipe(ttf2woff())
-    .pipe(dest(path.build.fonts));
-  return src(path.src.fonts)
-    .pipe(ttf2woff2())
-    .pipe(dest(path.build.fonts));
+  src(path.src.fonts).pipe(ttf2woff()).pipe(dest(path.build.fonts));
+  return src(path.src.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
 }
 
 gulp.task("otf2tt", function () {
-  return src([source_folder + '/fonts/*.otf'])
-    .pipe(fonter({
-      formats: ['ttf']
-    }))
-    .pipe(dest(source_folder + '/fonts/'));
-})
+  return src([source_folder + "/fonts/*.otf"])
+    .pipe(
+      fonter({
+        formats: ["ttf"],
+      })
+    )
+    .pipe(dest(source_folder + "/fonts/"));
+});
 
 gulp.task("svgSprite", function () {
-  return gulp.src([source_folder + " /inconsprite/*.svg"])
-    .pipe(svgSprite({
+  return gulp
+    .src([source_folder + " /inconsprite/*.svg"])
+    .pipe(
+      svgSprite({
         mode: {
           stack: {
             sprite: "../icons/icons.svg",
@@ -154,6 +156,28 @@ gulp.task("svgSprite", function () {
     )
     .pipe(dest(path.build.img));
 });
+
+function fontsStyle(params) {
+  let file_content = fs.readFileSync(source_folder + "/scss/fonts.scss");
+  if (file_content == "") {
+    fs.writeFile(source_folder + "/scss/fonts.scss", "", cb);
+    return fs.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split(".");
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fs.appendFile(source_folder + "/scss/fonts.scss",'@include font("' +fontname +'", "' +fontname +'","400","normal");\r\n',cb);
+          }
+          c_fontname = fontname;
+        }
+      }
+    });
+  }
+}
+
+function cb() {}
 
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
@@ -166,9 +190,10 @@ function clean(params) {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
